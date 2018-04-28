5283c9b95a10f035c2d07ffabf284125167eea99
Penguino <heriberto@melo.nyc>
2018-04-28 08:28:17 -0400

    Added name column to Plans, adjusted controller for new and form routes in plans

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,8 +1,18 @@
 class PlansController < ApplicationController
+  before_action :set_plan, only: [:show, :edit, :update, :destroy]
+
   def new
+    @plan = Plan.new
   end
 
   def create
+  end
+
+  def show
+  end
+
+  def index
+    @plans = Plan.all
   end
 
   def edit
@@ -12,5 +22,11 @@ class PlansController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_plan
+    @plan = Plan.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_28_121513) do
+ActiveRecord::Schema.define(version: 2018_04_28_122750) do
 
   create_table "plans", force: :cascade do |t|
     t.integer "time_per_workout"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_04_28_121513) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "day"
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
